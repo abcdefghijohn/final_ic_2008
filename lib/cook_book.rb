484f9f659b12783622291d6ecf7819dc44473838
John Kim <abcdefghijohnk@gmail.com>
2020-09-23 10:09:58 -0600

    can get ingredients test pass

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredient_names = []
+    @recipes.each do |recipe|
+      recipe.ingredients.each do |ingredient|
+        ingredient_names << ingredient.name
+      end
+    end
+    ingredient_names.uniq!
+  end
 end
